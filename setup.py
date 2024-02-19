d4b93e66adb67e9f21f5a5fcdd7f3fc18bfa8e71
tztian <tinaziyatian@gmail.com>
2024-02-19 16:38:41 -0500

    connected to mysql and created items table

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="DESKTOP-SFUR8TS",
-  user="tina",
-  password="tina"
+con = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password = "",
+  database = "bakemates"
 )
+
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS items")
+cur.execute('''CREATE TABLE items (
+                itemID VARCHAR(10) PRIMARY KEY,
+                price REAL
+              )''')
+
+cur.execute("SHOW TABLES")
+for x in cur:
+    print(x)
