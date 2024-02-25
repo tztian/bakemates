410dc3e50ad5197bf74cf9be9e01f035dd23e877
claramartinson <claramartinson@gmail.com>
2024-02-25 15:21:16 -0500

    started minimum set up on website so we have a starting point

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('landing.html')
 
 
 if __name__ == "__main__":
