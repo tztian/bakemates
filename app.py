08082fd34ac33cb790f2ec496c37ed2bdb36059e
claramartinson <claramartinson@gmail.com>
2024-04-01 16:20:14 -0400

    added claras stuff and removed old landing

--- app.py
+++ app.py
@@ -2,10 +2,45 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
 
+# Function to connect to MySQL database
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="bakemates"
+    )
+    return connection
+
 @app.route('/')
 def index():
     return render_template('landing.html')
 
+def listings():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT DISTINCT category FROM Item')  # Fetch distinct categories
+    categories = [row['category'] for row in cursor.fetchall()]
+    cursor.execute('SELECT * FROM Item')
+    items = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return render_template('listings.html', items=items, categories=categories)
+
+# Route to filter items by category for listings page
+@app.route('/filter', methods=['POST'])
+def filter_items():
+    category = request.form['category']
+    if category == 'All':
+        return redirect('/')
+    else:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM Item WHERE category = %s', (category,))
+        items = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return render_template('listings.html', items=items)
 
 if __name__ == "__main__":
     app.run(debug = True)
