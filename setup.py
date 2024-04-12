c6aba471a0786967fbf1170b922e8239be506747
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2024-04-12 12:20:11 -0400

    editing for aws connection

--- setup.py
+++ setup.py
@@ -1,57 +1,68 @@
 import mysql.connector
+from mysql.connector import Error
 
-con = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password = "",
-  database = "bakemates"
-)
+try:
+    con = mysql.connector.connect(
+        host="bakemates.cp600ck2epct.us-east-2.rds.amazonaws.com",
+        user="admin",
+        password="password",
+        database="bakemates",
+        connect_timeout=10
+    )
+    print("Successfully connected to database.")
+    cur = con.cursor()
 
-cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS Item")
+    cur.execute("DROP TABLE IF EXISTS Buyer")
+    cur.execute("DROP TABLE IF EXISTS Baker")
+    cur.execute("DROP TABLE IF EXISTS User")
 
-cur.execute("DROP TABLE IF EXISTS Item")
-cur.execute("DROP TABLE IF EXISTS Buyer")
-cur.execute("DROP TABLE IF EXISTS Baker")
-cur.execute("DROP TABLE IF EXISTS User")
+    # users table
+    cur.execute('''CREATE TABLE User
+                  (UserID VARCHAR(50) PRIMARY KEY,
+                    Email VARCHAR(50),
+                    Name VARCHAR(50),
+                    Phone VARCHAR(50),
+                    Age INT)''')
 
-# users table
-cur.execute('''CREATE TABLE User
-               (UserID VARCHAR(50) PRIMARY KEY,
-                Email VARCHAR(50),
-                Name VARCHAR(50),
-                Phone VARCHAR(50),
-                Age INT)''')
+    # Buyer table
+    cur.execute('''CREATE TABLE Buyer
+                  (BuyerID VARCHAR(50) PRIMARY KEY,
+                    Bio TEXT,
+                    FOREIGN KEY (BuyerID) REFERENCES User (UserID)
+                        ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
-# Buyer table
-cur.execute('''CREATE TABLE Buyer
-               (BuyerID VARCHAR(50) PRIMARY KEY,
-                Bio TEXT,
-                FOREIGN KEY (BuyerID) REFERENCES User (UserID)
-                    ON DELETE CASCADE ON UPDATE NO ACTION)''')
+    #Baker table
+    cur.execute('''CREATE TABLE Baker
+                  (BakerID VARCHAR(50) PRIMARY KEY,
+                    Address VARCHAR(50),
+                    Description TEXT,
+                    Rating DECIMAL(2,1),
+                    Website TEXT,
+                    FOREIGN KEY (BakerID) REFERENCES User (UserID)
+                        ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
-#Baker table
-cur.execute('''CREATE TABLE Baker
-               (BakerID VARCHAR(50) PRIMARY KEY,
-                Address VARCHAR(50),
-                Description TEXT,
-                Rating DECIMAL(2,1),
-                Website TEXT,
-                FOREIGN KEY (BakerID) REFERENCES User (UserID)
-                    ON DELETE CASCADE ON UPDATE NO ACTION)''')
-
-# item table
-cur.execute('''CREATE TABLE Item
-               (ItemID VARCHAR(50) PRIMARY KEY,
-                BakerID VARCHAR (50),
-                ItemCount INT,
-                ItemName TEXT,
-                ItemDescription TEXT,
-                Price FLOAT(2),
-                FOREIGN KEY (BakerID) REFERENCES Baker (BakerID)
-                    ON DELETE CASCADE ON UPDATE NO ACTION)''')
+    # item table
+    cur.execute('''CREATE TABLE Item
+                  (ItemID VARCHAR(50) PRIMARY KEY,
+                    BakerID VARCHAR (50),
+                    ItemCount INT,
+                    ItemName TEXT,
+                    ItemDescription TEXT,
+                    Price FLOAT(2),
+                    FOREIGN KEY (BakerID) REFERENCES Baker (BakerID)
+                        ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
 
 
-cur.execute("SHOW TABLES")
-for x in cur:
-    print(x)
+    cur.execute("SHOW TABLES")
+    for x in cur:
+        print(x)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if con.is_connected():
+        con.close()
+    if cur.is_connected():
+        cur.close()
+    print("MySQL connection is closed")
