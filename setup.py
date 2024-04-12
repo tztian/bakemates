445c4dc8cc0dabf79ebdfbd483b7cf459e34fb74
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2024-04-12 13:06:52 -0400

    added fake generated data from json and then am insertint that into the database

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import json
+
 
 con = mysql.connector.connect(
   host="localhost",
@@ -50,8 +52,45 @@ cur.execute('''CREATE TABLE Item
                 FOREIGN KEY (BakerID) REFERENCES Baker (BakerID)
                     ON DELETE CASCADE ON UPDATE NO ACTION)''')
 
+con.commit()
 
+# Load user data from users.json
+with open('./data/users.json', 'r') as file:
+    data = json.load(file)
+    users = data['users']
+    for user in users:
+        cur.execute('''
+        INSERT INTO User (UserID, Email, Name, Phone, Age)
+        VALUES (%s, %s, %s, %s, %s)
+        ''', (user['UserID'], user['Email'], user['Name'], user['Phone'], user['Age']))
+    con.commit()
+
+# Load baker data from bakers.json
+with open('./data/bakers.json', 'r') as file:
+    data = json.load(file)
+    bakers = data['bakers']
+    for baker in bakers:
+        cur.execute('''
+        INSERT INTO Baker (BakerID, Address, Description, Rating, Website)
+        VALUES (%s, %s, %s, %s, %s)
+        ''', (baker['BakerID'], baker['Address'], baker['Description'], baker['Rating'], baker['Website']))
+    con.commit()
+
+# Load and insert data from items.json
+with open('./data/items.json', 'r') as file:
+    data = json.load(file)
+    items = data['items']
+    for item in items:
+        cur.execute('''
+        INSERT INTO Item (ItemID, BakerID, ItemCount, ItemName, ItemDescription, Price)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ''', (item['ItemID'], item['BakerID'], item['ItemCount'], item['ItemName'], item['ItemDescription'], item['Price']))
+    con.commit()
 
 cur.execute("SHOW TABLES")
 for x in cur:
     print(x)
+
+#close cursors
+cur.close()
+con.close()
